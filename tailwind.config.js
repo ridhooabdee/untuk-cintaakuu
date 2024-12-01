242574b1d02fb5eccb943c19ca3d7fce48226241
Ridho Abdee Nugraha <ridhoabdeenugraha277@gmail.com>
2024-12-01 20:24:58 +0700

    fist deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,22 @@ module.exports = {
       padding: "16px",
     },
     extend: {
-      colors: { primary: "#14b8a6", dark: "#0f172a", secondary: "#64748b" },
+      colors: { primary: "#C73659", dark: "#171717", secondary: "#64748b"},
       screens: { "2xl": "1320px" },
+      
+      keyframes:{ 
+          ping :{
+          '75% 100%': {
+            transform: 'scale(3)',
+            opacity: '0'
+          }
+        }
+      },
+      animation:{ ping:'ping 0.8s cubic-bezier(0, 0, 0.2, 1) infinite'},
+      dropShadow: {
+        '2xl': '0 35px 35px rgba(199, 54, 89, 0.50)',
+        'lumayan': '0 5px 10px rgba(199, 54, 89, 0.50)',
+      }
     },
   },
   plugins: [],
